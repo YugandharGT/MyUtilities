799b55700a9321cadf0ab57c675e019039b020ff
Yugandhar T <tyugandhar158@outlook.com>
2024-02-09 09:09:22 +0530

    fixed namespace after changing framework version

--- MyUtilities/Program.cs
+++ MyUtilities/Program.cs
@@ -9,19 +9,19 @@ namespace MyUtilities
     {
         static void Main(string[] args)
         {
-            var plainPwd = "Sekar8@yuga";
-            var hashedPwd = SecurityOperations.EncryptString(plainPwd);
-            Console.WriteLine($"Encrypted pwd is: {hashedPwd}");
-            var decodedPwd = SecurityOperations.DecryptString(hashedPwd);
-            Console.WriteLine($"Encrypted pwd is: {decodedPwd}");
+            //var plainPwd = "Sekar8@yuga";
+            //var hashedPwd = SecurityOperations.EncryptString(plainPwd);
+            //Console.WriteLine($"Encrypted pwd is: {hashedPwd}");
+            //var decodedPwd = SecurityOperations.DecryptString(hashedPwd);
+            //Console.WriteLine($"Encrypted pwd is: {decodedPwd}");
 
-            // string sourcePath = ConfigurationManager.AppSettings["PdfToWordFile"];
-            // if (!File.Exists(sourcePath))
-            // {
-            //     Console.WriteLine("Configured file not exists in the path");
-            //     Console.Read();
-            // }
-            //WordConverter.ConvertsPdfToWord(sourcePath);
+            string sourcePath = ConfigurationManager.AppSettings["PdfToWordFile"];
+            if (!File.Exists(sourcePath))
+            {
+                Console.WriteLine("Configured file not exists in the path");
+                Console.Read();
+            }
+            WordConverter.ConvertsPdfToWord(sourcePath);
 
             //OCRConversion.ImagetoPdfConvertion(sourcePath);
 
