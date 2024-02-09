5f1edde9a307f0e36006ae9e62e2026b318611d2
Yugandhar T <tyugandhar158@outlook.com>
2024-02-09 09:06:00 +0530

    targeted to .net 8.0 and removed OCR package

--- MyUtilities/OCRConversion.cs
+++ MyUtilities/OCRConversion.cs
@@ -1,5 +1,5 @@
-﻿using Patagames.Ocr;
-using Patagames.Ocr.Enums;
+﻿//using Patagames.Ocr;
+//using Patagames.Ocr.Enums;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -10,16 +10,16 @@ namespace MyUtilities
     {
         public static void ImagetoPdfConvertion(string imageFilePath)
         {
-            using (var api = OcrApi.Create())
-            {
-                api.Init(Languages.English);
-                using (var renderer = OcrPdfRenderer.Create("multipagepdffile", @"D:\"))
-                {
-                    renderer.BeginDocument("Title");
-                    api.ProcessPages(imageFilePath, null, 0, renderer); //@"C:\Tapas\multidocs.tif"
-                    renderer.EndDocument();
-                }
-            }
+            //using (var api = OcrApi.Create())
+            //{
+            //    api.Init(Languages.English);
+            //    using (var renderer = OcrPdfRenderer.Create("multipagepdffile", @"D:\"))
+            //    {
+            //        renderer.BeginDocument("Title");
+            //        api.ProcessPages(imageFilePath, null, 0, renderer); //@"C:\Tapas\multidocs.tif"
+            //        renderer.EndDocument();
+            //    }
+            //}
         }
     }
 }
